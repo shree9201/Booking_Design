3be273fdc487314b2e7fe5d5d7f2c79c85e65da9
shree9201 <109940669+shree9201@users.noreply.github.com>
2022-09-06 22:11:14 +0530

    Base Code

---
+++ js/ie8.js
@@ -0,0 +1,4 @@
+/*
+ * Title:   ConnectBooker-Google Direct Booking Link - IE8 Javascript
+ * Author:  https://connectbooker.com
+ */
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/*
+ * Title:   ConnectBooker-Google Direct Booking Link - Custom Javascript file
+ * Author:  https://connectbooker.com
+ */
+
+tjq(document).ready(function() {
+    // UI Form Element
+});
